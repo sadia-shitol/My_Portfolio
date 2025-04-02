758495f3242f17181739e552eaaba5ad64912353
SadiaShitol <shitol3080@gmail.com>
2025-04-02 08:01:21 +0600

    updated the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next'
 import { useState, useEffect } from 'react'
 import './globals.css'
+import { Poppins } from 'next/font/google' // Change to your preferred font
+
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
 
 export const metadata: Metadata = {
   title: 'Sadia Shitol',
@@ -16,11 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="inter.className">   
+      <body className="font-serif">   
          {children}
 
       </body>
+      {/* <body className={`${poppins.className} ${poppins.variable}`}>
+        {children}
+      </body> */}
     </html>
   )
 }
+
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        serif: ['"Times New Roman"', 'Times', 'serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
